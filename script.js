aac9ff1d9ac4fac843e7bb7fa1d910b235e4f816
XDMcShots <75882939+XDMcShots@users.noreply.github.com>
2021-02-17 12:57:23 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+var randNum = Math.floor(Math.random()*25)+1;
+document.querySelector(".randomNum").innerText = randNum;
+
+if (randNum == 1)
+{
+  document.querySelector(".randomName").innerText = "Ananya!";
+}
+if (randNum == 2)
+{
+  document.querySelector(".randomName").innerText = "Anisha!";
+}
+if (randNum == 3)
+{
+  document.querySelector(".randomName").innerText = "Eisa!";
+}
+if (randNum == 4)
+{
+  document.querySelector(".randomName").innerText = "Ishaan!";
+}
+if (randNum == 5)
+{
+  document.querySelector(".randomName").innerText = "Jacob!";
+}
+if (randNum == 6)
+{
+  document.querySelector(".randomName").innerText = "Janhvi!";
+}
+if (randNum == 7)
+{
+  document.querySelector(".randomName").innerText = "Jessica!";
+}
+if (randNum == 8)
+{
+  document.querySelector(".randomName").innerText = "Kriti!";
+}
+if (randNum == 9)
+{
+  document.querySelector(".randomName").innerText = "Mitansh!";
+}
+if (randNum == 10)
+{
+  document.querySelector(".randomName").innerText = "Neha!";
+}
+if (randNum == 11)
+{
+  document.querySelector(".randomName").innerText = "Neil!";
+}
+if (randNum == 12)
+{
+  document.querySelector(".randomName").innerText = "Oyeshi!";
+}
+if (randNum == 13)
+{
+  document.querySelector(".randomName").innerText = "Paul!";
+}
+if (randNum == 14)
+{
+  document.querySelector(".randomName").innerText = "Raaghhav!";
+}
+if (randNum == 15)
+{
+  document.querySelector(".randomName").innerText = "Radhika!";
+}
+if (randNum == 16)
+{
+  document.querySelector(".randomName").innerText = "Samarth Sashidhar! (The fossil dude)";
+}
+if (randNum == 17)
+{
+  document.querySelector(".randomName").innerText = "Samarth Singh Juneja!";
+}
+if (randNum == 18)
+{
+  document.querySelector(".randomName").innerText = "Sarrah!";
+}
+if (randNum == 19)
+{
+  document.querySelector(".randomName").innerText = "Simran!";
+}
+if (randNum == 20)
+{
+  document.querySelector(".randomName").innerText = "Srishti!";
+}
+if (randNum == 21)
+{
+  document.querySelector(".randomName").innerText = "Swarit!";
+}
+if (randNum == 22)
+{
+  document.querySelector(".randomName").innerText = "Tannay!";
+}
+if (randNum == 23)
+{
+  document.querySelector(".randomName").innerText = "Vedika!";
+}
+if (randNum == 24)
+{
+  document.querySelector(".randomName").innerText = "Yashita!";
+}
+if (randNum == 25)
+{
+  document.querySelector(".randomName").innerText = "Yuvraj!";
+}
